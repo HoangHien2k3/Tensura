c81b1fb65be4e99c246b4d5fd7dcd896416ded0f
HoangHien2k3 <hoangn1626@gmail.com>
2025-05-22 01:01:53 +0700

    Update Modules.lua

--- Storage/Modules.lua
+++ Storage/Modules.lua
@@ -19,6 +19,18 @@ Modules.PR = function(msg)
     warn(FunText[1] .. " " .. msg)
 end
 
+Modules.RandomString = function(length)
+    local chars = "abcdefghijklmnopqrstuvwxyz0123456789"
+    local randomString = ""
+    
+    for i = 1, length do
+        local randomIndex = math.random(1, #chars)
+        randomString = randomString .. chars:sub(randomIndex, randomIndex)
+    end
+    
+    return randomString
+end
+
 Modules.CorrectName = function(plr_name)
     for _, v in ipairs(Players:GetPlayers()) do
         if string.lower(v.Name):sub(1, #plr_name) == string.lower(plr_name) or
@@ -58,8 +70,8 @@ Modules.DevConsole = function()
 end
 
 Modules.TP = function(local_plr, plr_name)
-    if plr_name and plr_name.Character and plr_name.Character:FindFirstChild("HumanoidRootPart") then
-        Players[local_plr].Character:SetPrimaryPartCFrame(plr_name.Character.HumanoidRootPart)
+    if Players[plr_name] and Players[plr_name].Character and Players[plr_name].Character:FindFirstChild("HumanoidRootPart") then
+        Players[local_plr].Character:SetPrimaryPartCFrame(Players[plr_name].Character.HumanoidRootPart)
     else
         print(FunText[1] .. " " .. plr_name .. " does not exist a " .. BodyPart[1])
     end
